d9149520de7d655420f424acc6b141053090bc88
Khang Quach <kadfwfsfsvs@gmail.com>
2026-06-29 18:27:18 +0700

    feat(base): add error pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,9 @@ import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "@/features/auth/pages/LoginPage";
 import RegisterPage from "@/features/auth/pages/RegisterPage";
 import DashboardPage from "@/pages/DashboardPage";
+import ForbiddenPage from "@/pages/ForbiddenPage";
 import NotFoundPage from "@/pages/NotFoundPage";
+import ServerErrorPage from "@/pages/ServerErrorPage";
 import { appConstants } from "@/share/constants/appConstants";
 
 export const router = createBrowserRouter([
@@ -17,6 +19,14 @@ export const router = createBrowserRouter([
   {
     path: appConstants.REGISTER,
     element: <RegisterPage />,
+  },
+  {
+    path: appConstants.FORBIDDEN,
+    element: <ForbiddenPage />,
+  },
+  {
+    path: appConstants.SERVER_ERROR,
+    element: <ServerErrorPage />,
   },
   {
     path: appConstants.NOT_FOUND,
